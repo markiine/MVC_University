3747689ed9cb72682abcb15d350958922d0d96ee
Opilane TTHK <opilane@tthk.ee>
2022-10-06 15:05:46 +0300

    migratsions&SQL

---
+++ University/Controllers/StudentsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using University.Data;
+
+namespace University.Controllers
+{
+    public class StudentsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public StudentsController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var result = await _context.Students.ToListAsync();
+
+            return View(result);
+        }
+    }
+}
