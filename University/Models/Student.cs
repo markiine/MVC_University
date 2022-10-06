da131a7e4f6b9ce65b599d2275c5d4ddc541c61f
Opilane TTHK <opilane@tthk.ee>
2022-10-06 13:28:42 +0300

    SchoolContext

--- University/Models/Student.cs
+++ University/Models/Student.cs
@@ -11,6 +11,7 @@ namespace University.Models
         public string LastName { get; set; }
         public string FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
+
         public ICollection<Enrollment> Enrollments { get; set; }
 
     }
