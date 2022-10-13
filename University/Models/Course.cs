ebc8e71bb401e8de9e3c75c156baaee616f6afda
Opilane TTHK <opilane@tthk.ee>
2022-10-13 13:09:07 +0300

    Update Course.cs

--- University/Models/Course.cs
+++ University/Models/Course.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace University.Models
 {
     public class Course
     {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public int CourseId { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
