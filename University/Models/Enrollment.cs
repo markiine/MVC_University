79f3f6092a33c55b3e9214e1077894502df7fe61
Opilane TTHK <opilane@tthk.ee>
2022-10-20 15:28:10 +0300

    ComplexDataModel

--- University/Models/Enrollment.cs
+++ University/Models/Enrollment.cs
@@ -1,10 +1,14 @@
-﻿namespace University.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace University.Models
 {
     public class Enrollment
     {
         public int EnrollmentId { get; set; }
         public int CourseId { get; set; }
         public int StudentId { get; set; }
+        [DisplayFormat(NullDisplayText = "No grade")]
         public Grade? Grade { get; set; }
 
         public Course Course { get; set; }
