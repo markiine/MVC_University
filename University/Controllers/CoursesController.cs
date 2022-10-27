76319533c0a35bb0f0fab81257000cbcc0f9948e
Opilane TTHK <opilane@tthk.ee>
2022-10-27 14:45:06 +0300

    Instructors

---
+++ University/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using University.Data;
+
+namespace University.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public CoursesController(SchoolContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<IActionResult>Index()
+        {
+            var courses = _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking();
+            return View(await courses.ToListAsync());
+        }
+    }
+}
---
+++ University/Controllers/InstructorsController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using University.Data;
+using University.Models;
+using University.Models.SchoolViewModels;
+
+namespace University.Controllers
+{
+    public class InstructorsController : Controller
+    {
+        private readonly SchoolContext _context;
+
+        public InstructorsController(SchoolContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index(int? id, int? courseId)
+        {
+            var viewModel = new InstructorIndexData();
+            viewModel.Instructors = await _context.Instructors
+                  .Include(i => i.OfficeAssignment)
+                  .Include(i => i.CourseAssignments)
+                    .ThenInclude(i => i.Course)
+                        .ThenInclude(i => i.Enrollments)
+                            .ThenInclude(i => i.Student)
+                  .Include(i => i.CourseAssignments)
+                    .ThenInclude(i => i.Course)
+                        .ThenInclude(i => i.Department)
+                  .AsNoTracking()
+                  .OrderBy(i => i.LastName)
+                  .ToListAsync();
+
+            if (id != null)
+            {
+                ViewData["InstructorId"] = id.Value;
+                Instructor instructor = viewModel.Instructors.Where(
+                    i => i.Id == id.Value).Single();
+                viewModel.Courses = instructor.CourseAssignments.Select(s => s.Course);
+            }
+
+            if (courseId != null)
+            {
+                ViewData["CourseId"] = courseId.Value;
+                viewModel.Enrollments = viewModel.Courses.Where(
+                    x => x.CourseId == courseId).Single().Enrollments;
+            }
+
+            return View(viewModel);
+        }
+    }
+}
